f376b40fda6b6888e1d3bf2f4d97aa84d1493833
Evgeny Cherkashin <eugeneai@irnok.net>
2020-02-26 04:31:42 +0800

    Added examples for utilities and a proxy multi adapter.

--- lab1.py
+++ lab1.py
@@ -1,7 +1,11 @@
+import sqlite3 as sql
 from zope.interface import Interface, Attribute, implementer
-from zope.component import adapter, getGlobalSiteManager
+from zope.component import adapter, getGlobalSiteManager, \
+    getUtility, getAdapter, getMultiAdapter
 from collections import OrderedDict
 from nose.tools import raises
+from zope.interface.interfaces import ComponentLookupError
+import re
 
 # Интерфейсы - явным образм описанный контракт между компонентами
 
@@ -60,6 +64,7 @@ class VirtualCatalog(object):
 
 
 class IImage(Interface):
+    """Describes images."""
     def width():
         """Returns the width of the image."""
     def heigh():
@@ -68,6 +73,23 @@ class IImage(Interface):
         """Returns byte per pixel size."""
     def content():
         """Returns the matrix of the image."""
+
+
+class IDBConnection(Interface):
+    """Connection to a database"""
+
+
+class IUser(Interface):
+    name = Attribute("Name of the user")
+
+
+class IUserCredentials(Interface):
+    """User credentials"""
+
+    def check(interface, method):
+        """Checks wether user is allowed to send message
+        identified by `interface` and `method` name.
+        Value of `method` can be a reference or string name."""
 
 
 @implementer(IImage)
@@ -110,8 +132,74 @@ class AdapterOfIImageToIFile(object):
         return img.width()*img.heigh()*img.bpp()
 
 
+@adapter(IImage, IUserCredentials)
+@implementer(IImage)
+class CredentialCheckingProxyForIImage(object):
+    def __init__(self, context, usercred):
+        self.context = context
+        self.usercred = usercred
+
+    def heigh(self):
+        self.usercred.check(self.context, "heigh")
+        return self.context.heigh()
+
+    def width(self):
+        self.usercred.check(self.context, "width")
+        return self.context.width()
+
+    def bpp(self):
+        self.usercred.check(self.context, "bpp")
+        return self.context.bpp()
+
+
 GSM = getGlobalSiteManager()
 GSM.registerAdapter(AdapterOfIImageToIFile)
+GSM.registerAdapter(CredentialCheckingProxyForIImage, name="user-cred")
+conn = sql.connect(":memory:")
+GSM.registerUtility(component=conn, provided=IDBConnection, name="sqlite-conn")
+del conn
+
+
+class AccessDeniedException(Exception):
+    def __init__(self, interface, method, message=None):
+        self.interface = interface
+        self.method = method
+        self.message = message
+
+    def __str__(self):
+        if self.message:
+            msg = self.message
+        else:
+            msg = ''
+        msg = self.__class__.__name__ + \
+            "("+msg+"for \"{}\" in interface {})"
+        return msg.format(str(self.method), self.interface)
+
+
+@implementer(IUser, IUserCredentials)
+class TestUser(object):
+    def __init__(self, name, interface, regexp=None):
+        self.name = name
+        self.interface = interface
+        if isinstance(regexp, str):
+            self.regexp = re.compile(regexp)
+        else:
+            self.regexp = regexp  # Including None
+
+    def check(self, context, method):
+        if isinstance(method, str):
+            pass
+        else:
+            method = method.__name__
+
+        if not self.interface.providedBy(context):
+            raise AccessDeniedException(self.interface, method,
+                                        "interface is not provided with object")
+
+        m = self.regexp.match(method)
+        if m:
+            return True
+        raise AccessDeniedException(self.interface, method, "access denied ")
 
 
 class TestVirtualCatalog(object):
@@ -135,3 +223,33 @@ class TestVirtualCatalog(object):
         self.c.add(i)
         # assert(self.c.files[self.f1.name] == self.f1)
         print(self.c.list())
+
+
+class TestMarkerInterfaceStuff(object):
+    def setUp(self):
+        self.conn = getUtility(IDBConnection, name="sqlite-conn")
+        self.tu = TestUser(name="John Lee", interface=IImage, regexp=r"^.*h$")
+        self.img = VirtualImage(10, 10, 32)
+        self.imgprox = getMultiAdapter((self.img, self.tu),
+                                       IImage, name="user-cred")
+
+    def test_sqlite_conn_not_none(self):
+        assert(self.conn is not None)
+
+    @raises(ComponentLookupError)
+    def test_wrong_name(self):
+        getUtility(IDBConnection)
+
+    def test_cred_obj(self):
+        self.tu.check(self.img, self.img.heigh)
+
+    @raises(AccessDeniedException)
+    def test_cred_obj_failed(self):
+        self.tu.check(self.img, self.img.bpp)
+
+    def test_access_to_imgproxy(self):
+        self.imgprox.heigh()
+
+    @raises(AccessDeniedException)
+    def test_access_to_imgproxy_failed(self):
+        self.imgprox.bpp()
